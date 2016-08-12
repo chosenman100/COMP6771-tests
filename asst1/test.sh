500ec25432d23f3066bcb7c047c7c7e3bcbf03d2
Karl Krauth <karl.krauth@gmail.com>
2016-08-12 21:35:07 +1000

    Added runtime info and a longest word test.

--- asst1/test.sh
+++ asst1/test.sh
@@ -3,7 +3,7 @@
 DICT_NAME="EnglishWords.dat"
 OUT_EXEC_FILE="out-exec.txt"
 OUT_TEST_FILE="out-test.txt"
-HEADER="Enter start word (RETURN to quit): Enter destination word: Found ladder: "
+HEADER="Enter start word (RETURN to quit): Enter destination word: "
 
 if [[ $# -ne 1 ]]
 then
@@ -29,13 +29,22 @@ do
     fi
 
     # Format output test file to include the heading.
+    # Else if the test file doesn't exist we assume that no ladders exist.
     echo -n "$HEADER" > "$OUT_TEST_FILE"
-    cat "out-$TEST_CASE" >> "$OUT_TEST_FILE"
+    if [ -e "out-$TEST_CASE" ]
+    then
+        echo -n "Found ladder: " >> "$OUT_TEST_FILE"
+        cat "out-$TEST_CASE" >> "$OUT_TEST_FILE"
+    else
+        echo "No ladder found." >> "$OUT_TEST_FILE"
+    fi
 
     # Run the test.
+    START=$(date +%s.%N | awk '{print int($1 * 1000)}')
     $EXECUTABLE_PATH < "in-$TEST_CASE" > "$OUT_EXEC_FILE"
+    TIME=$(( $(date +%s.%N | awk '{print int($1 * 1000)}') - $START ))
     diff -b -B "$OUT_TEST_FILE" "$OUT_EXEC_FILE"
-    echo "Done"
+    echo "Done in $TIME ms".
 done
 
 if [ -x "${EXECUTABLE_PATH}_ref" ]; then
