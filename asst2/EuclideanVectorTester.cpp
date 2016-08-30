8f59a62b24d33e59752b5288aa5bd3dc5c55ebcb
Karl Krauth <karl.krauth@gmail.com>
2016-08-30 22:51:27 +1000

    Added tests provided in the spec.

--- asst2/EuclideanVectorTester.cpp
+++ asst2/EuclideanVectorTester.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
-#include "constructor_tests.h"
+#include "ConstructorTests.h"
+#include "ProvidedTests.h"
 
 int main(int argc, char *argv[]) {
     std::cout << "Starting test suite.\n";
 
-    std::cout << "Running constructor_tests.cpp\n";
+    std::cout << "Running provided tests.\n";
+    provided_tests();
+    std::cout << "Passed provided tests.\n";
+
+    std::cout << "Running constructor tests.\n";
     constructor_tests();
-    std::cout << "Passed constructor_tests.cpp\n";
+    std::cout << "Passed constructor tests.\n";
 
     std::cout << "All tests passed.\n";
 
