474e1a932647a9d4995b17e1fedb93c30aea612a
Timothy <chosenman100@users.noreply.github.com>
2016-09-01 16:29:40 +1000

    Fixed Wrong Answers

--- asst2/AccessTests.cpp
+++ asst2/AccessTests.cpp
@@ -61,13 +61,13 @@ void access_tests() {
     {
         // Simple unit vector.
         auto vec = evec::EuclideanVector(4, 1.0).createUnitVector();
-        assertEquals(vec, {0.25, 0.25, 0.25, 0.25});
+        assertEquals(vec, {0.5, 0.5, 0.5, 0.5});
     }
 
     {
         // Unit vector from const.
-        const evec::EuclideanVector vec{4.0, 9.0};
-        assertEquals(vec.createUnitVector(), {4.0 / 5.0, 9.0 / 5.0});
+        const evec::EuclideanVector vec{4.0, 3.0};
+        assertEquals(vec.createUnitVector(), {4.0 / 5.0, 3.0 / 5.0});
     }
 
     {
